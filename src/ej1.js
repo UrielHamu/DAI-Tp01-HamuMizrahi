decf35176c8cf0bdc39004cae0a7f059abb7f20f
Android Studio <devandroid@ort>
2026-03-16 10:42:02 -0300

    primer..[

---
+++ src/ej1.js
@@ -0,0 +1,11 @@
+let textoEntrada01 = "Escuela", textoEntrada02 = "ORT";
+let textoSalida = "";
+textoSalida = concatInvert(textoEntrada01, textoEntrada02);
+console.clear();
+console.log(`Textos de Entrada: "${textoEntrada01}" y "${textoEntrada02}"`);
+console.log(`Texto de Salida: "${textoSalida}"`);
+function concatInvert (texto1, texto2){
+let returnValue = `${texto1}${texto2}`;
+returnValue=returnValue.split('').reverse().join('');
+return returnValue;
+}
---
+++ src/ej2.js
@@ -0,0 +1,6 @@
+import {PI, sumar} from './modules/matematica.js';
+let total, numero1=10, numero2=20;
+console.clear();
+console.log(`La constante PI vale '${PI}'`);
+total = sumar(numero1, numero2);
+console.log(`sumar(${numero1}, ${numero2}) = ${total}`);
