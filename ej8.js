2a953237ba3caf3a1991dbdfb45be5ef626030cc
URIEL FEDERICO HAMU <49518164@ort>
2026-03-31 11:04:52 -0300

    habia una parte comentada, ahora si está terminado

--- ej8.js
+++ ej8.js
@@ -6,12 +6,10 @@ let resultado = null;
 resultado  = await OMDBSearchByPage("cars", 1);
 console.log("OMDBSearchByPage", resultado);
 
-/*
+
 resultado = await OMDBSearchComplete("cars");
 console.log("OMDBSearchComplete", resultado);
-*/
 
-//resultado = await OMDBGetByImdbID("tt0317219");
-//console.log("OMDBGetByImdbID", resultado);
 
-console.log(resultado);
+resultado = await OMDBGetByImdbID("tt0317219");
+console.log("OMDBGetByImdbID", resultado);
